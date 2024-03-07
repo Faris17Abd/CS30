1d746db733147c22fb1ec875a4e9fde8331470fc
Faris17Abd <faris_taweel@hotmail.com>
2024-03-07 15:20:24 -0600

    MultiColourGrid

---
+++ MultiColorGrid/sketch.js
@@ -0,0 +1,19 @@
+// MultiColorGrid
+// Fares Abdalla
+// 03/05/2024
+// Buildinig a grid that so happens to be multi colored.
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  squareBack();
+}
+
+function squareBack(){
+  square(windowWidth, windowHeight, 40)
+} 
