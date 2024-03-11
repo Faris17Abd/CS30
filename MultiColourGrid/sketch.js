58b4725de83538b83c19be8a5b087ea55eb99166
Faris17Abd <faris_taweel@hotmail.com>
2024-03-11 15:10:52 -0600

    Terrain

--- MultiColourGrid/sketch.js
+++ MultiColourGrid/sketch.js
@@ -3,17 +3,26 @@
 // 03/05/2024
 // Buildinig a grid that so happens to be multi colored.
 
-
+let rectwidth = 20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(255);
+  rectMode(CENTER);
+  drawRectangles();
 }
 
 function draw() {
-  background(220);
-  squareBack();
+  
 }
 
-function squareBack(){
-  square(windowWidth, windowHeight, 40)
-} 
+function drawRectangles(){
+  let rectHeight;
+  fill(0)
+  for(let x = 0; x < width; x += rectwidth){
+    // rectHeight = x;
+    rectHeight = random(0, height*0.8);
+    rect(x, height/2, rectwidth, rectHeight);
+  }
+
+}
---
+++ New Challenge/sketch.js
@@ -0,0 +1,20 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+circletime = 100
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  fill(140,180,255);
+  circle(200,200,circletime);
+  fill(220,220,200);
+  circle(200,200,circletime/2);
+}
