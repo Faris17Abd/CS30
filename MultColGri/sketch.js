652f6f927156559bb2657f60e6e93f12b89f4fde
Faris17Abd <faris_taweel@hotmail.com>
2024-03-13 15:17:16 -0600

    Update sketch.js

--- MultColGri/sketch.js
+++ MultColGri/sketch.js
@@ -10,40 +10,44 @@ y = 0
 squaresize = 50
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(220);
+  createCanvas(400, 400);
   squareback();
-  mouseClicked();
+  document.addEventListener("contextmenu", event => event.preventDefault())
 }
 
-function mouseClicked(){
-  if(mouseButton === LEFT && mouseIsPressed){
-    if(squaresize < 10){
+function keyPressed() {
+  squareback();
+}
+
+
+function mousePressed() {
+  print (mouseButton)
+  if (mouseButton === LEFT) {
+    if (squaresize < 11) {
       squaresize -= 0;
-    }else {
-      squaresize -= 1;
+    } else {
+      squaresize = squaresize * 2;
     }
-    if(mouseButton === RIGHT && mouseIsPressed){
-      if(squaresize > 200){
-        squaresize -= 0;
-      }else {
-        squaresize += 1;
-      }
-}
   }
+  if (mouseButton === RIGHT) {
+    if (squaresize > 399) {
+      squaresize -= 0;
+    } else {
+      squaresize = squaresize / 2;
+    }
+  }
+  squareback();
 }
-function squareback(){
-  for(let x = 0; x < width; x += squaresize){
-    for(let y = 0; y < height; y += squaresize){
-      let redV = random(0,255);
-      let greenV = random(0,255);
-      let blueV = random(0,255);
+
+function squareback() {
+  for (let x = 0; x < width; x += squaresize) {
+    for (let y = 0; y < height; y += squaresize) {
+      let redV = random(0, 255);
+      let greenV = random(0, 255);
+      let blueV = random(0, 255);
       fill(redV, greenV, blueV);
-      square(x,y,squaresize);
-      
+      square(x, y, squaresize);
+
     }
   }
 }
