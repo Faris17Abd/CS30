2098fc9e1ef5dda6c0203e85495cd85f5640832a
Faris17Abd <faris_taweel@hotmail.com>
2024-03-14 12:32:10 -0600

    Update sketch.js

--- terrain generator assignment/sketch.js
+++ terrain generator assignment/sketch.js
@@ -1,15 +1,31 @@
-// Project Title
-// Your Name
-// Date
+// Terrain 
+// Fares Abdalla
+// 03/11/2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
+let rectwidth = 20;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(255);
+  rectMode(CENTER);
+  drawRectangles();
 }
 
 function draw() {
-  background(220);
+  
+}
+
+function drawRectangles(){
+  let rectHeight;
+  fill(0)
+  for(let x = 0; x < width; x += rectwidth){
+    // rectHeight = x;
+    rectHeight = random(0, height*0.8);
+    rect(x, height/2, rectwidth, rectHeight);
+  }
+
 }
