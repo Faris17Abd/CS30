3d552bc93501292d2ec3ba7b4d91a6652e803aca
Faris17Abd <faris_taweel@hotmail.com>
2024-04-16 15:22:02 -0600

    ccar

---
+++ ccar/sketch.js
@@ -0,0 +1,27 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  drawroad();
+}
+
+
+function drawroad(){
+  fill(0)
+  rect(0,height/2, width, 200);
+  rect(0,height/2 - 200, width, 200);
+  for (let i = 0; i < width; i++) {
+    fill(255)
+    rect(i*50, height/2 - 10, 30, 20);
+  fill( )
+}
+}
