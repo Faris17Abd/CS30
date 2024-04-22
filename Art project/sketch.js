68c3bbcb0ee906092293fb80c7f0f8960615ee92
Faris17Abd <faris_taweel@hotmail.com>
2024-04-21 23:24:14 -0600

    Update sketch.js

--- Art project/sketch.js
+++ Art project/sketch.js
@@ -5,11 +5,12 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let period = 0.1;
+let peak = 110;
+let spacesBetween = 150;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
-  background(220);
-}
+
