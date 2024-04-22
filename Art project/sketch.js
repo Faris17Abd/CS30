780dff401ca9aabc3ed8739a188987dcfead35ef
Faris17Abd <faris_taweel@hotmail.com>
2024-04-21 23:38:10 -0600

    Update sketch.js

--- Art project/sketch.js
+++ Art project/sketch.js
@@ -11,6 +11,19 @@ let spacesBetween = 150;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  angleModes(DEGREES);
 }
 
 
+function drawingtheSinsiods(){
+  // This coding loops so that the there will be spaces between each line
+  for(let i = 100; i >= 0; i--){
+    period - 0.1;
+    peak += 4;
+    trough += 4;
+  }
+}
+
+function drawSinewaves(){
+  
+}
