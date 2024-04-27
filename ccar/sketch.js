b562176e7fe3aa2b83c20191e8229c890b668f82
Faris17Abd <faris_taweel@hotmail.com>
2024-04-27 10:30:07 -0600

    Update sketch.js

--- ccar/sketch.js
+++ ccar/sketch.js
@@ -36,6 +36,37 @@ function drawroad(){
 }
 
 class colon3{
-  constructor(x,y)
-    
+  constructor(x,y){
+    this.x = x
+    if(y === 1){
+      this.y = random(height / 4, height / 2-20);
+    }
+    else{
+      this.y = random(height / 2 + 5, height - height / 4 - 15);
+    }
+    this.direction_of_car = y;
+    this.speed_of_car = 5
+    this.colour_of_car = color(random(255),random(255), random (255));
+    this.Type_of_vehicle = Math.floor(random(0,2));
+    this.The_World = 0;
+  }  
+
+  display(){
+    noStroke();
+    if(this.Type_of_vehicle === 1){
+      this.car();
+    }
+    else{
+      this.truck();
+    }
+  }
+  car(){
+    fill(this.color);
+    rect(this.x, this.y, 30, 15);
+    fill(200, 230, 255);
+    rect(this.x + 19.3, this.y - 4, 10, 5);
+    rect(this.x, this.y - 4, 10, 5);
+    rect(this.x + 19.3, this.y  + 15, 10, 5);
+    rect(this.x, this.y + 15, 10, 5);
+  }
 }
