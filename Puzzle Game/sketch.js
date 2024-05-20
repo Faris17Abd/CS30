84d821eccb97cce14bacd6cbba6dbe664c9d8c33
Faris17Abd <faris_taweel@hotmail.com>
2024-05-20 02:23:40 -0600

    Puzzle game

---
+++ Puzzle Game/sketch.js
@@ -0,0 +1,20 @@
+// puzzle game
+// Fares Abdalla
+// May 20th, 2024
+
+
+let numsrows = 4;
+let numscols = 5;
+let rectWidth, rectHeight;
+let griddata = [[]];
+let won = [false];
+let mousepostion = [];
+ 
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
