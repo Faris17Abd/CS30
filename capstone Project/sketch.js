a08b9f41645684ecc0d050262e76c868509fa3a8
Faris17Abd <faris_taweel@hotmail.com>
2024-06-10 00:39:57 -0600

    Update sketch.js

--- capstone Project/sketch.js
+++ capstone Project/sketch.js
@@ -184,35 +184,38 @@ function Tetris_Grid_drawing_outline(){
     for(let x =0 ; x<NUM_COLS; x++){
       rectMode(CORNER);
       let type = tetris_outline[y][x]
-      if(type === 0){
-        fill(255);
-      }
-      else if(type === 1){
-        fill(255, 0, 0);
-      }
-      else if(type === 2){
-        fill(0, 255, 0);
-      }
-      else if(type === 3){
-        fill(127,0,255);
-      }
-      else if(type === 4){
-        fill(255,255,0);
-      }
-      else if(type === 5){
-        fill(255,128,0);
-      }
-      else if(type === 6){
-        fill(0,102,204);
-      }
-      else if(type === 7){
-        fill(51,255,255);
-      }
       rect(x*squareSize + 200 , y*squareSize + 200, squareSize);
     }  
   }
 }
 
+
+function getColor(type){ // this makes it easier to access the colors
+  if (type === 0) {
+    return color(255);
+  } 
+  else if (type === 1) {
+    return color(255, 0, 0);
+  } 
+  else if (type === 2) {
+    return color(0, 255, 0);
+  }
+   else if (type === 3) {
+    return color(127, 0, 255);
+  }
+   else if (type === 4) {
+    return color(255, 255, 0);
+  }
+   else if (type === 5) {
+    return color(255, 128, 0);
+  }
+   else if (type === 6) {
+    return color(0, 102, 204);
+  }
+   else if (type === 7) {
+    return color(51, 255, 255);
+  }
+}
 function isitcolliding(x, y, block){
   for(let i = 0; i < block.length; i++){ // goes through each row of the block
     for(let j = 0; i < block[i].length; j++) // goes through each colum of current row
